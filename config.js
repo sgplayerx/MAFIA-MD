060a6de62b640ca6f86a410c6fbf26749ed1f923
sgplayerx <sgxhacker323@gmail.com>
2025-09-10 21:11:20 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,48 @@
+
+// change only what you are ask to change else bit won't work thanks for your understanding 
+const fs = require('fs'), 
+      dotenv = fs.existsSync('config.env') ? require('dotenv').config({ path: '/.env' }) : undefined,
+      convertToBool = (text, fault = 'true') => text === fault;
+
+global.session = "https://mafia-md-d43a6ac59abd.herokuapp.com/"; 
+ 
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "MAFIA-MD~hETMsPVK7nwRBa4NzqcM", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
+ANTI_DELETE: process.env.ANTI_DELETE || "inboxonly", // can be set to inboxonly/allchats/true/false
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas
+AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by MAFIA-MD", // // Input Yours custom...
+MODE: process.env.MODE || "public", // Put private or public or inbox or groups
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923192433518", // Only 1 owner Number Here, others Add to sudo numbers...
+OWNER_NAME: process.env.OWNER_NAME || "MAFIA-INSIDE ‹³策", // Input Yours custom...(Maintain font for Flow)
+PACK_AUTHOR: process.env.PACK_AUTHOR || "", // Added // Input Yours custom...
+PACK_NAME: process.env.PACK_NAME || "", // Added // Input Yours custom...
+PREFIX: process.env.PREFIX || ".",
+VERSION: process.env.VERSION || "6.0.0",
+ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
+ANTICALL: process.env.ANTICALL || "false",
+ANTIBAD: process.env.ANTIBAD || "false",
+BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
+ANTICALL_MSG: process.env.ANTICALL_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+BOT_NAME: process.env.BOT_NAME || "𓆩ု᪳MAFIA-MDှ᪳𓆪", //  don't change 
+BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/ldg6o2.jpg", //  don't change 
+AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
+AUTO_BIO: process.env.AUTO_BIO || "false",
+AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "MAFIA MD ALIVE",
+CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
+WELCOME: process.env.WELCOME || "false",
+//not working for the moment do don't on it
+GOODBYE: process.env.GOODBYE || "false", //not working for the moment do don't on it
+AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
+AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799", // Add Multiple Country Codes Separated by Comma...
+PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
+TIME_ZONE: process.env.TIME_ZONE || "Asia/Karachi", // Enter yours else leave blank if not sure
+};
+
+let file = require.resolve(__filename); 
+fs.watchFile(file, () => { fs.unwatchFile(file); console.log(`Update '${__filename}'`); delete require.cache[file]; require(file); });
+// That's All...
